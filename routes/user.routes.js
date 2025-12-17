3bfa663cd598dab7a8984e09244f187a4352cf23
varsha-badda <varshabadda0@gmail.com>
2025-12-17 11:23:59 +0530

    errors found

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,13 +7,19 @@ router.get('/register',(req,res)=>{
     res.render('register')
 })
 router.post('/register',
-    body('email').trim().isEmail(),
+    body('email').trim().isEmail().isLength({min:13}),
     body('username').trim().isLength({min:3}),
     body('password').trim().isLength({min:5}),
     (req,res)=>{
         const errors = validationResult(req);
-    console.log(req.body)
-    res.send("user registered")
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message:'Invalid data'
+
+            })
+        }
+    res.send(errors)
 })
 
 
