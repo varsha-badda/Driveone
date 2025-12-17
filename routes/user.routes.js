620fb070c89bf55d4ad662d1b3c1d4ffb4716d9c
varsha-badda <varshabadda0@gmail.com>
2025-12-17 22:17:22 +0530

    login

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {body,validationResult} = require('express-validator');
 const userModel = require("../models/user.model")
 const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
 
 router.get('/register',(req,res)=>{
     res.render('register')
@@ -30,6 +31,54 @@ router.post('/register',
     res.json(newUser)
 })
 
+router.get('/login',(req,res)=>{
+    res.render('login')
+})
+router.post('/login',
+    body('username').trim().isLength({min:13}),
+    body('password').trim().isLength({min:5}),
+    async(req,res)=>{
+
+
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                error:errors.array(),
+                message:'invalid data'
+            })
+        }
+        const {username,password} = req.body;
+
+        const user = await userModel.findOne({
+            username:username
+        })
+        if(!user){
+            return res.status(400).json({
+                message:'username or password is invalid'
+                
+            })
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({
+                message:'username or password is invalid'
+            })
+        }
+        const token = jwt.sign({
+            userId:user._id,
+            email:user.email,
+            username:user.username
+
+        },
+        process.env.JWT_SECRET,
+    )
+    res.json({
+        token
+    })
+
+    }
+
+)
 
 
 
